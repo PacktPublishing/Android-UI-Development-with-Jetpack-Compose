86ef4e028f6d9a4cfd1cc1095f22d9b8e6a6c486
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-08-04 10:46:03 +0200

    New sample app added

---
+++ chapter_02/HelloView/app/src/main/java/eu/thomaskuenneth/composebook/hello_view/MainActivity.kt
@@ -0,0 +1,44 @@
+package eu.thomaskuenneth.composebook.hello_view
+
+import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import eu.thomaskuenneth.composebook.hello_view.databinding.MainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.message.text = getString(R.string.welcome)
+        binding.name.run {
+            setOnEditorActionListener { _, _, _ ->
+                binding.done.performClick()
+                true
+            }
+            doAfterTextChanged {
+                enableOrDisableButton()
+            }
+            visibility = VISIBLE
+            binding.done.visibility = VISIBLE
+        }
+        binding.done.setOnClickListener {
+            val name = binding.name.text
+            if (name.isNotBlank()) {
+                binding.message.text = getString(R.string.hello, name)
+                binding.name.visibility = GONE
+                it.visibility = GONE
+            }
+        }
+        enableOrDisableButton()
+    }
+
+    private fun enableOrDisableButton() {
+        binding.done.isEnabled = binding.name.text.isNotEmpty()
+    }
+}
---
+++ chapter_02/README.md
@@ -0,0 +1,3 @@
+# Chapter 2: Understanding the “Declarative” Paradigm
+
+<img style="-webkit-filter: drop-shadow(5px 5px 5px #222); filter: drop-shadow(5px 5px 5px #222)" src="assets/hello_view.png" width="20%" />
