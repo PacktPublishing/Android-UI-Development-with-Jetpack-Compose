f8ee04b80aff10533828e88d1003dd75a72bbdc6
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-09-08 14:00:34 +0200

    Update ModifierDemoActivity.kt

--- chapter_03/ModifierDemo/app/src/main/java/eu/thomaskuenneth/composebook/modifierdemo/ModifierDemoActivity.kt
+++ chapter_03/ModifierDemo/app/src/main/java/eu/thomaskuenneth/composebook/modifierdemo/ModifierDemoActivity.kt
@@ -10,10 +10,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.DrawModifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 class ModifierDemoActivity : ComponentActivity() {
@@ -21,9 +26,17 @@ class ModifierDemoActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             // OrderDemo()
-            TextWithYellowBackground(
+//            TextWithYellowBackground(
+//                text = "Hello Compose",
+//                modifier = Modifier.padding(32.dp)
+//            )
+            Text(
                 text = "Hello Compose",
-                modifier = Modifier.padding(32.dp)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .drawYellowCross(),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h1
             )
         }
     }
@@ -57,3 +70,23 @@ fun TextWithYellowBackground(
         modifier = modifier.background(Color.Yellow)
     )
 }
+
+fun Modifier.drawYellowCross() = then(
+    object : DrawModifier {
+        override fun ContentDrawScope.draw() {
+            drawLine(
+                color = Color.Yellow,
+                start = Offset(0F, 0F),
+                end = Offset(size.width - 1, size.height - 1),
+                strokeWidth = 10F
+            )
+            drawLine(
+                color = Color.Yellow,
+                start = Offset(0F, size.height - 1),
+                end = Offset(size.width - 1, 0F),
+                strokeWidth = 10F
+            )
+            drawContent()
+        }
+    }
+)
