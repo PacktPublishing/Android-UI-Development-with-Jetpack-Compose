8cfd0823d9d7f39c27c3e04718c5621025cb9ee0
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-09-08 11:41:18 +0200

    Project ModifierDemo added

---
+++ chapter_03/ModifierDemo/app/src/main/java/eu/thomaskuenneth/composebook/modifierdemo/ModifierDemoActivity.kt
@@ -0,0 +1,43 @@
+package eu.thomaskuenneth.composebook.modifierdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class ModifierDemoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OrderDemo()
+        }
+    }
+}
+
+@Composable
+fun OrderDemo() {
+    var color by remember { mutableStateOf(Color.Blue) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
+            .border(BorderStroke(width = 2.dp, color = color))
+            .background(Color.LightGray)
+            .clickable {
+                color = if (color == Color.Blue)
+                    Color.Red
+                else
+                    Color.Blue
+            }
+    )
+}
