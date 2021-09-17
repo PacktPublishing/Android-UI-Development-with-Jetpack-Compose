379c766b2323aa5dcb6760409bcba3543a6021f8
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-09-17 12:16:27 +0200

    CheckboxWithLabel added

--- chapter_04/PredefinedLayoutsDemo/app/src/main/java/eu/thomaskuenneth/composebook/predefinedlayoutsdemo/PredefinedLayoutsDemoActivity.kt
+++ chapter_04/PredefinedLayoutsDemo/app/src/main/java/eu/thomaskuenneth/composebook/predefinedlayoutsdemo/PredefinedLayoutsDemoActivity.kt
@@ -3,11 +3,65 @@ package eu.thomaskuenneth.composebook.predefinedlayoutsdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-class ColorPickerDemoActivity : ComponentActivity() {
+class PredefinedLayoutsDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            PredefinedLayoutsDemo()
         }
+    }
+}
+
+@Composable
+@Preview
+fun PredefinedLayoutsDemo() {
+    val red = remember { mutableStateOf(false) }
+    val green = remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        CheckboxWithLabel(
+            label = stringResource(id = R.string.red),
+            state = red
+        )
+    }
+}
+
+@Composable
+fun CheckboxWithLabel(label: String, state: MutableState<Boolean>) {
+    Row(
+        modifier = Modifier.clickable {
+            state.value = !state.value
+        }, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = state.value,
+            onCheckedChange = {
+                state.value = it
+            }
+        )
+        Text(
+            text = label,
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
 }
