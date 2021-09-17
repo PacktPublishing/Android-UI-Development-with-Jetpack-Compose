87b5a1da6374ef51bae76fe803cfe6c321d7407d
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-09-17 12:26:36 +0200

    Finished example

--- chapter_04/PredefinedLayoutsDemo/app/src/main/java/eu/thomaskuenneth/composebook/predefinedlayoutsdemo/PredefinedLayoutsDemoActivity.kt
+++ chapter_04/PredefinedLayoutsDemo/app/src/main/java/eu/thomaskuenneth/composebook/predefinedlayoutsdemo/PredefinedLayoutsDemoActivity.kt
@@ -3,11 +3,9 @@ package eu.thomaskuenneth.composebook.predefinedlayoutsdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,8 +31,9 @@ class PredefinedLayoutsDemoActivity : ComponentActivity() {
 @Composable
 @Preview
 fun PredefinedLayoutsDemo() {
-    val red = remember { mutableStateOf(false) }
-    val green = remember { mutableStateOf(false) }
+    val red = remember { mutableStateOf(true) }
+    val green = remember { mutableStateOf(true) }
+    val blue = remember { mutableStateOf(true) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -43,6 +43,43 @@ fun PredefinedLayoutsDemo() {
             label = stringResource(id = R.string.red),
             state = red
         )
+        CheckboxWithLabel(
+            label = stringResource(id = R.string.green),
+            state = green
+        )
+        CheckboxWithLabel(
+            label = stringResource(id = R.string.blue),
+            state = blue
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 16.dp)
+        ) {
+            if (red.value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Red)
+                )
+            }
+            if (green.value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                        .background(Color.Green)
+                )
+            }
+            if (blue.value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(64.dp)
+                        .background(Color.Blue)
+                )
+            }
+        }
     }
 }
 
