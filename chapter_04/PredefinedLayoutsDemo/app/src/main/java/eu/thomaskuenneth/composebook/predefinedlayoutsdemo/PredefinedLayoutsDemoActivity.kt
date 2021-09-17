de24d572948a0aec3c81db204e80b073923e5a65
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-09-17 11:06:27 +0200

    Started new chapter and added project PredefinedLayoutsDemo

---
+++ chapter_04/PredefinedLayoutsDemo/app/src/main/java/eu/thomaskuenneth/composebook/predefinedlayoutsdemo/PredefinedLayoutsDemoActivity.kt
@@ -0,0 +1,13 @@
+package eu.thomaskuenneth.composebook.predefinedlayoutsdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ColorPickerDemoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
