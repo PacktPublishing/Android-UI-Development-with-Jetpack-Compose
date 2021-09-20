12b828a245a9422cd484d64125d276bfe03e8bc5
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-09-20 18:30:16 +0200

    Update CustomLayoutDemoActivity.kt

--- chapter_04/CustomLayoutDemo/app/src/main/java/eu/thomaskuenneth/composebook/customlayoutdemo/CustomLayoutDemoActivity.kt
+++ chapter_04/CustomLayoutDemo/app/src/main/java/eu/thomaskuenneth/composebook/customlayoutdemo/CustomLayoutDemoActivity.kt
@@ -3,41 +3,95 @@ package eu.thomaskuenneth.composebook.customlayoutdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random
 
 @ExperimentalComposeUiApi
 class ConstraintLayoutDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColumnWithTexts()
+            CustomLayoutDemo()
         }
     }
 }
 
 @Composable
 @Preview
-fun ColumnWithTexts() {
-    Column {
-        Text(
-            text = "Android UI development with Jetpack Compose",
-            style = MaterialTheme.typography.h3,
-        )
-        Text(
-            text = "Hello Compose",
-            style = MaterialTheme
-                .typography.h5.merge(TextStyle(color = Color.Red))
-        )
+fun CustomLayoutDemo() {
+    SimpleFlexBox {
+        for (i in 0..42) {
+            ColoredBox()
+        }
     }
+}
+
+@Composable
+fun ColoredBox() {
+    Box(
+        modifier = Modifier
+            .border(
+                width = 2.dp,
+                color = Color.Black
+            )
+            .background(randomColor())
+            .width((40 * randomInt123()).dp)
+            .height((10 * randomInt123()).dp)
+    )
+}
+
+@Composable
+fun SimpleFlexBox(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content,
+        measurePolicy = simpleFlexboxMeasurePolicy()
+    )
+}
+
+private fun simpleFlexboxMeasurePolicy(): MeasurePolicy =
+    MeasurePolicy { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var yPosition = 0
+            var xPos = 0
+            var maxY = 0
+            placeables.forEach { placeable ->
+                if (xPos + placeable.width > constraints.maxWidth) {
+                    xPos = 0
+                    yPosition += maxY
+                    maxY = 0
+                }
+                placeable.placeRelative(x = xPos, y = yPosition)
+                xPos += placeable.width
+                if (maxY < placeable.height) {
+                    maxY = placeable.height
+                }
+            }
+        }
+    }
+
+private fun randomInt123() = Random.nextInt(1, 4)
+
+private fun randomColor() = when (randomInt123()) {
+    1 -> Color.Red
+    2 -> Color.Green
+    else -> Color.Blue
 }
