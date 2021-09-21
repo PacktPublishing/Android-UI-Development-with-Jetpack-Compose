284a3328a9c1ae7958290b59ad1a75531d70bae2
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-09-21 12:02:18 +0200

    Minor refactoring

--- chapter_04/CustomLayoutDemo/app/src/main/java/eu/thomaskuenneth/composebook/customlayoutdemo/CustomLayoutDemoActivity.kt
+++ chapter_04/CustomLayoutDemo/app/src/main/java/eu/thomaskuenneth/composebook/customlayoutdemo/CustomLayoutDemoActivity.kt
@@ -71,7 +71,7 @@ private fun simpleFlexboxMeasurePolicy(): MeasurePolicy =
             constraints.maxWidth,
             constraints.maxHeight
         ) {
-            var yPosition = 0
+            var yPos = 0
             var xPos = 0
             var maxY = 0
             placeables.forEach { placeable ->
@@ -79,12 +79,12 @@ private fun simpleFlexboxMeasurePolicy(): MeasurePolicy =
                     constraints.maxWidth
                 ) {
                     xPos = 0
-                    yPosition += maxY
+                    yPos += maxY
                     maxY = 0
                 }
                 placeable.placeRelative(
                     x = xPos,
-                    y = yPosition
+                    y = yPos
                 )
                 xPos += placeable.width
                 if (maxY < placeable.height) {
