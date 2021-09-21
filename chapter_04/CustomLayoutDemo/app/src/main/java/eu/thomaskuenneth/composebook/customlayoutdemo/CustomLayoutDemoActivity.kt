272b0191e90253753796f210781afdfd292cde4e
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-09-21 11:39:49 +0200

    Update CustomLayoutDemoActivity.kt

--- chapter_04/CustomLayoutDemo/app/src/main/java/eu/thomaskuenneth/composebook/customlayoutdemo/CustomLayoutDemoActivity.kt
+++ chapter_04/CustomLayoutDemo/app/src/main/java/eu/thomaskuenneth/composebook/customlayoutdemo/CustomLayoutDemoActivity.kt
@@ -67,17 +67,25 @@ private fun simpleFlexboxMeasurePolicy(): MeasurePolicy =
         val placeables = measurables.map { measurable ->
             measurable.measure(constraints)
         }
-        layout(constraints.maxWidth, constraints.maxHeight) {
+        layout(
+            constraints.maxWidth,
+            constraints.maxHeight
+        ) {
             var yPosition = 0
             var xPos = 0
             var maxY = 0
             placeables.forEach { placeable ->
-                if (xPos + placeable.width > constraints.maxWidth) {
+                if (xPos + placeable.width >
+                    constraints.maxWidth
+                ) {
                     xPos = 0
                     yPosition += maxY
                     maxY = 0
                 }
-                placeable.placeRelative(x = xPos, y = yPosition)
+                placeable.placeRelative(
+                    x = xPos,
+                    y = yPosition
+                )
                 xPos += placeable.width
                 if (maxY < placeable.height) {
                     maxY = placeable.height
