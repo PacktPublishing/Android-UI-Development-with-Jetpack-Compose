26b582e85517971818bee5cc855a221a3c6d7e7b
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-10-02 17:12:01 +0200

    Update StateDemoActivity.kt

--- chapter_05/StateDemo/app/src/main/java/eu/thomaskuenneth/composebook/statedemo/StateDemoActivity.kt
+++ chapter_05/StateDemo/app/src/main/java/eu/thomaskuenneth/composebook/statedemo/StateDemoActivity.kt
@@ -3,19 +3,59 @@ package eu.thomaskuenneth.composebook.statedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import java.util.*
+import kotlin.random.Random
 
 class StateDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PredefinedLayoutsDemo()
+            RememberWithKeyDemo()
         }
     }
 }
 
 @Composable
 @Preview
-fun PredefinedLayoutsDemo() {
+fun RememberWithKeyDemo() {
+    var key by remember { mutableStateOf(false) }
+    val date by remember(key) { mutableStateOf(Date()) }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(date.toString())
+        Button(onClick = { key = !key }) {
+            Text(text = stringResource(id = R.string.click))
+        }
+    }
+}
+
+@Composable
+@Preview
+fun SimpleStateDemo1() {
+    val num = remember { mutableStateOf(Random.nextInt(0, 10)) }
+    Text(text = num.value.toString())
+}
+
+@Composable
+@Preview
+fun SimpleStateDemo2() {
+    val num by remember { mutableStateOf(Random.nextInt(0, 10)) }
+    Text(text = num.toString())
+}
+
+@Composable
+@Preview
+fun SimpleStatelessComposable1() {
+    Text(text = "Hello Compose")
+}
+
+@Composable
+fun SimpleStatelessComposable2(text: State<String>) {
+    Text(text = text.value)
 }
