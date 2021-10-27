c724e9b14b56a62ccf529ab3f95b0555b804d723
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-10-27 12:13:25 +0200

    LaunchedEffectDemo() added

--- chapter_07/EffectDemo/app/src/main/java/eu/thomaskuenneth/composebook/effectdemo/EffectDemoActivity.kt
+++ chapter_07/EffectDemo/app/src/main/java/eu/thomaskuenneth/composebook/effectdemo/EffectDemoActivity.kt
@@ -3,11 +3,71 @@ package eu.thomaskuenneth.composebook.effectdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 class EffectDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            LaunchedEffectDemo()
         }
+    }
+}
+
+@Composable
+@Preview
+fun LaunchedEffectDemo() {
+    var clickCount by rememberSaveable { mutableStateOf(0) }
+    var counter by rememberSaveable { mutableStateOf(0) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Button(onClick = {
+                clickCount += 1
+            }) {
+                Text(
+                    text = if (clickCount == 0)
+                        stringResource(id = R.string.start)
+                    else
+                        stringResource(id = R.string.restart)
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(enabled = clickCount > 0,
+                onClick = {
+                    clickCount = 0
+                }) {
+                Text(text = stringResource(id = R.string.stop))
+            }
+            if (clickCount > 0) {
+                LaunchedEffect(clickCount) {
+                    counter = 0
+                    while (isActive) {
+                        counter += 1
+                        delay(1000)
+                    }
+                }
+            }
+        }
+        Text(
+            text = "$counter",
+            style = MaterialTheme.typography.h3
+        )
     }
 }
