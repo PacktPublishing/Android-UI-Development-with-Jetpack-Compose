0dee1e81624d6279f9f1b81befdcf4ed1a0b6034
Thomas Künneth <thomas@thomaskuenneth.eu>
2021-11-01 15:38:53 +0100

    Simple animation added

--- chapter_08/AnimationDemo/app/src/main/java/eu/thomaskuenneth/composebook/animationdemo/AnimationDemoActivity.kt
+++ chapter_08/AnimationDemo/app/src/main/java/eu/thomaskuenneth/composebook/animationdemo/AnimationDemoActivity.kt
@@ -3,9 +3,22 @@ package eu.thomaskuenneth.composebook.animationdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@ExperimentalAnimationApi
 class AnimationDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +28,32 @@ class AnimationDemoActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 @Preview
 fun AnimationDemo() {
+    var visible by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            visible = !visible
+        }) {
+            Text(stringResource(id = R.string.click))
+        }
+        AnimatedVisibility(
+            visible = visible
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier.padding(top = 32.dp),
+                style = MaterialTheme.typography.h4
+            )
+        }
+    }
 }
