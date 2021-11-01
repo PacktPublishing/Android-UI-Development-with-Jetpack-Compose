8137523a76d35ff43f81e089e7ff43cd143f9ac4
Thomas Künneth <thomas@thomaskuenneth.eu>
2021-11-01 15:27:18 +0100

    Project AnimationDemo added

---
+++ chapter_08/AnimationDemo/app/src/main/java/eu/thomaskuenneth/composebook/animationdemo/AnimationDemoActivity.kt
@@ -0,0 +1,21 @@
+package eu.thomaskuenneth.composebook.animationdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class AnimationDemoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimationDemo()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun AnimationDemo() {
+}
