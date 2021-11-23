cfcfa4f7db36ee4e308854394ef6b50893173704
Thomas Künneth <thomas@thomaskuenneth.eu>
2021-11-23 15:22:47 +0100

    Update TestingAndDebuggingDemoActivity.kt

--- chapter_10/TestingAndDebuggingDemo/app/src/main/java/eu/thomaskuenneth/composebook/testinganddebuggingdemo/TestingAndDebuggingDemoActivity.kt
+++ chapter_10/TestingAndDebuggingDemo/app/src/main/java/eu/thomaskuenneth/composebook/testinganddebuggingdemo/TestingAndDebuggingDemoActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -29,8 +28,7 @@ class TestingAndDebuggingDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // SimpleButtonDemo()
-            ImageDemo()
+            SimpleButtonDemo()
         }
     }
 }
