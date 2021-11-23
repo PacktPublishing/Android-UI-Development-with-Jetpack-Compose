771fbd70143ffce5a0ed6cdcd2489cd9316953f5
Thomas Künneth <thomas@thomaskuenneth.eu>
2021-11-23 12:12:46 +0100

    Refactoring

--- chapter_10/TestingAndDebuggingDemo/app/src/main/java/eu/thomaskuenneth/composebook/testinganddebuggingdemo/TestingAndDebuggingDemoActivity.kt
+++ chapter_10/TestingAndDebuggingDemo/app/src/main/java/eu/thomaskuenneth/composebook/testinganddebuggingdemo/TestingAndDebuggingDemoActivity.kt
@@ -3,7 +3,6 @@ package eu.thomaskuenneth.composebook.testinganddebuggingdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
@@ -11,7 +10,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 
 fun isEven(num: Int): Boolean {
@@ -28,42 +26,19 @@ class TestingAndDebuggingDemoActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun SimpleButtonDemo() {
-    var text by remember { mutableStateOf("A") }
+    val a = stringResource(id = R.string.a)
+    val b = stringResource(id = R.string.b)
+    var text by remember { mutableStateOf(a) }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Button(onClick = {
-            text = if (text == "A") "B" else "A"
+            text = if (text == a) b else a
         }) {
             Text(text = text)
-        }
-    }
-}
-
-
-val COLOR1 = Color.White
-val COLOR2 = Color.LightGray
-
-@Composable
-fun SimpleButtonDemo2() {
-    var color by remember { mutableStateOf(COLOR1) }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = color),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(onClick = {
-            color = if (color == COLOR1)
-                COLOR2
-            else
-                COLOR1
-        }) {
-            Text(text = stringResource(id = R.string.toggle))
         }
     }
 }
